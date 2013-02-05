b384dd67e75acf3c00d7a15732d639d16615ecf6
CharlesRandles <charles.randles@gmail.com>
2013-02-05 21:06:10 +1000

    On to day 2...

--- ruby/day1.rb
+++ ruby/day1.rb
@@ -1,2 +1,31 @@
 #!/usr/bin/ruby
 
+puts "Hello, World!"
+
+source="Hello, Ruby"
+target="Ruby"
+idx=source.index(target)
+if idx==nil then idx=" nowhere" end
+
+puts ("In the string \"#{source}\" the string \"#{target}\" appears at index #{idx}.")
+
+name="Charles"
+
+10.times {puts name}
+
+10.times {|i| puts "This is sentence number #{i+1}"}
+
+n = rand(100)
+puts "I've thought of a number. Have a guess:"
+
+guess = gets.to_i
+
+while guess != n
+  if n > guess then
+    puts "Higher..."
+  else
+    puts "Lower..."
+  end
+  guess = gets.to_i
+end
+puts "Correct!"
