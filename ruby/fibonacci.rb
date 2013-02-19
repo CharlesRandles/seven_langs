5219e8eca17fe2bcaa58896fa2c98feba8808e50
CharlesRandles <charles.randles@gmail.com>
2013-02-19 10:42:41 +1000

    Update ruby/fibonacci.rb
    Wrapped in a class to keep count

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,28 +1,40 @@
-def bad_fib(n)
-  print("\tin bad_fib(#{n})\n")
-  if n < 2 then
-    1
-  else
-    bad_fib(n-1) + bad_fib(n-2)
-  end
-end
+class Fib_Counter
+  attr_accessor :count
 
-def good_fib_helper(a,b,n)
-  print("\tin good_fib_helper(#{n})\n")
-  if n <= 2 then
-    b
-  else
-    good_fib_helper(b, a+b, n-1)
+  def initialize()
+    @count = 0
   end
-end
 
-def good_fib(n)
-  good_fib_helper(1,1,n+1)
+  def bad_fib(n)
+    @count = @count + 1
+    print("\tin bad_fib(#{n})\n")
+    if n < 2 then
+      1
+    else
+      bad_fib(n-1) + bad_fib(n-2)
+    end
+  end
+
+  def good_fib_helper(a,b,n)
+    @count = @count + 1
+    print("\tin good_fib_helper(#{n})\n")
+    if n <= 2 then
+      b
+    else
+      good_fib_helper(b, a+b, n-1)
+    end
+  end
+
+  def good_fib(n)
+    good_fib_helper(1,1,n+1)
+  end
 end
 
 n=7
-print("bad_fib(#{n}) = #{bad_fib(n)}\n")
+bad = Fib_Counter.new
+print("bad_fib(#{n}) = #{bad.bad_fib(n)}\n")
+print("#{bad.count} calls.\n")
 print("***********************************\n")
-print("good_fib(#{n}) = #{good_fib(n)}\n")d_fib(n)}\n")
-print("***********************************")
-print("good_fib(#{n}) = #{good_fib(n)}\n")
+good=Fib_Counter.new
+print("good_fib(#{n}) = #{good.good_fib(n)}\n")
+print("#{good.count} calls.\n")
