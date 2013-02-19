45ec9526957d480523742b8cff878d42d727902d
CharlesRandles <charles.randles@gmail.com>
2013-02-19 10:32:02 +1000

    Update ruby/fibonacci.rb
    Fixed off-by-1 error and daft bug.

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -9,18 +9,20 @@ end
 
 def good_fib_helper(a,b,n)
   print("\tin good_fib_helper(#{n})\n")
-  if n < 2 then
-    1
+  if n <= 2 then
+    b
   else
-    good_fib_helper(b, a+1, n-1)
+    good_fib_helper(b, a+b, n-1)
   end
 end
 
 def good_fib(n)
-  good_fib_helper(1,1,n)
+  good_fib_helper(1,1,n+1)
 end
 
 n=7
 print("bad_fib(#{n}) = #{bad_fib(n)}\n")
+print("***********************************\n")
+print("good_fib(#{n}) = #{good_fib(n)}\n")d_fib(n)}\n")
 print("***********************************")
 print("good_fib(#{n}) = #{good_fib(n)}\n")
